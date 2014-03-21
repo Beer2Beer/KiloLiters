8c95c9bc3c158effff14f535ffe228cda25a68f9
Federico Bertoli <thebertozz@gmail.com>
2014-03-21 18:33:55 +0100

    Other improvements to location services

--- KiloLiters/app/src/main/java/it/beer2beer/kiloliters/FragmentRicercaStazioni.java
+++ KiloLiters/app/src/main/java/it/beer2beer/kiloliters/FragmentRicercaStazioni.java
@@ -1,43 +1,35 @@
 package it.beer2beer.kiloliters;
 
-import android.app.AlertDialog;
+
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.view.InflateException;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
-import android.app.AlertDialog;
+
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.location.LocationClient;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
 
 
 /**
  * Created by federico on 08/03/14.
  */
+
 public class FragmentRicercaStazioni extends Fragment implements LocationListener {
 
   //  private LocationRequest locationRequest;
@@ -48,6 +40,7 @@ public class FragmentRicercaStazioni extends Fragment implements LocationListene
     double lat;
     double lng;
     View view;
+    boolean toastLocalizzazioneVisualizzato = false;
 
     public FragmentRicercaStazioni() {
     }
@@ -62,7 +55,6 @@ public class FragmentRicercaStazioni extends Fragment implements LocationListene
                 parent.removeView(view);
         }
     */
-
             view = inflater.inflate(R.layout.view_ricerca_stazioni, container,
                     false);
             if (googleMap == null) {
@@ -108,7 +100,40 @@ public class FragmentRicercaStazioni extends Fragment implements LocationListene
             public void onFocusChange(View view, boolean b) {
                 ((InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
                         view.getWindowToken(), 0);
-            }
+
+                GoogleMap.OnMyLocationButtonClickListener locationButtonClickListener = new GoogleMap.OnMyLocationButtonClickListener() {
+                    @Override
+                    public boolean onMyLocationButtonClick() {
+
+                        if (!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) &&
+                                !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+                            Context context = getActivity();
+                            Toast t = Toast.makeText(context, "Per poter utilizzare al meglio il servizio di Ricerca Distributori, " +
+                                    "attivare la localizzazione", Toast.LENGTH_LONG);
+                            t.show();
+
+                        }
+
+                        return true;
+                    }
+                };
+
+                googleMap.getMap().setOnMyLocationButtonClickListener(locationButtonClickListener);
+
+                if (!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) &&
+                        !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+                    if (!toastLocalizzazioneVisualizzato) {
+                        Context context = getActivity();
+                        Toast t = Toast.makeText(context, "Per poter utilizzare al meglio il servizio di Ricerca Distributori, " +
+                                "attivare la localizzazione", Toast.LENGTH_LONG);
+                        t.show();
+                        toastLocalizzazioneVisualizzato = true;
+                    }
+
+                }
+        }
         });
 
         return view;
@@ -132,7 +157,6 @@ public class FragmentRicercaStazioni extends Fragment implements LocationListene
         latLng = new LatLng(latitude,longitude);
 
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(latLng);
-
         googleMap.getMap().animateCamera(cameraUpdate);
 
     }
@@ -156,23 +180,8 @@ public class FragmentRicercaStazioni extends Fragment implements LocationListene
     private void updatePlaces(){
         //get location manager
 
-
-
         try {
-
             locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-            if (!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) &&
-                    !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-
-                if (googleMap.getUserVisibleHint() == true) {
-                    Context context = getActivity();
-                    Toast t = Toast.makeText(context, "Abilitare servizi di localizzazione", Toast.LENGTH_LONG);
-                    t.show();
-                }
-
-            }
-
         } catch (NullPointerException e) {
 
             e.printStackTrace();
@@ -187,6 +196,7 @@ public class FragmentRicercaStazioni extends Fragment implements LocationListene
                 locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100, 0, this);
 
                 Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
                 if (lastKnownLocation != null) {
                     lat = lastKnownLocation.getLatitude();
                     lng = lastKnownLocation.getLongitude();
@@ -216,7 +226,7 @@ public class FragmentRicercaStazioni extends Fragment implements LocationListene
      //   if(userMarker!=null) userMarker.remove();
         //create and set marker properties
       /*
-       userMarker = theMap.addMarker(new MarkerOptions()
+       userMarker = googleMap.addMarker(new MarkerOptions()
                 .position(lastLatLng)
                 .title("You are here")
                 .icon(BitmapDescriptorFactory.fromResource(userIcon))
@@ -231,7 +241,7 @@ public class FragmentRicercaStazioni extends Fragment implements LocationListene
                 "json?location="+lat+","+lng+
                 "&radius=1000&sensor=true" +
                 "&types=gas_station"+
-                "&key=AIzaSyA7ttpFSTYMgDsan6GvsznzN-xkhN1M8n0";//ADD KEY
+                "&key=AIzaSyA7ttpFSTYMgDsan6GvsznzN-xkhN1M8n0";
 
         //execute query
       //  new GetPlaces().execute(placesSearchStr);
@@ -258,15 +268,5 @@ public class FragmentRicercaStazioni extends Fragment implements LocationListene
             locationManager.removeUpdates(this);
         }
     }
-
-/*
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-        if (isVisibleToUser) {
-
-        }
-        else {  }
-    }
-*/
 }
+
